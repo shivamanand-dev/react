6bc6726d0b3e73828449c3bfab1296aa725068a4
Shivam0Anand <shivam_anand@outlook.com>
2020-01-20 05:27:40 +0530

    made other component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, {
-  Component
-} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
+import person from './Person/Person';
 
 class App extends Component {
   render() {
@@ -10,6 +10,8 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I am Shivam</h1>
         <p>Friend of Kavya!</p>
+
+        <Person />
       </div>
     );
     // return React.createElement('div', {className: App}, React.createElement('h1', null, 'From react.create element!!'))
---
+++ src/Person/Person.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+var person = ()=>{
+    return <p>I'm a Person!</p>
+}
+
+export default person;
