811a267e63c4eb7abc982baba10b3da79226c65c
Shivam0Anand <shivam_anand@outlook.com>
2020-01-24 00:31:32 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons : [
+      {name : "Shivam", age: 22},
+      {name : "Kavya", age: 21}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
@@ -12,8 +20,8 @@ class App extends Component {
 
         <button>Switch name</button>
 
-        <Person name="Shivam" age="22"> Loves Kavya</Person>
-        <Person name="Kavya" age="21"/>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> Loves {this.state.persons[1].name}</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
       </div>
     );
     // return React.createElement('div', {className: App}, React.createElement('h1', null, 'From react.create element!!'))
