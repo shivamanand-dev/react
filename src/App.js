fe49f14a9bf38ece3f0daf98c313b70579884321
shivam0anand <shivam_anand@outlook.com>
2020-02-21 23:17:46 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ const app = props=> {
   const [ personsState, setPersonsState ] = useState({
     persons : [
       {name : "Shivam", age: 22},
-      {name : "Kavya", age: 21}
+      {name : "Kavya", age: 21},
+      {name: "Shilpi", age: 28}
     ],
     otherState: 'some other value'
   })
@@ -34,12 +35,19 @@ const app = props=> {
         <button onClick={switchNameHandler}>Switch name</button>
 
         <Person 
-        name={personsState.persons[0].name} 
-        age={personsState.persons[0].age}>
-        Loves {personsState.persons[1].name}</Person>
+        name={ personsState.persons[0].name } 
+        age={ personsState.persons[0].age }
+        click={ switchNameHandler }>
+        Loves { personsState.persons[1].name }</Person>
+
         <Person 
         name={personsState.persons[1].name} 
         age={personsState.persons[1].age}/>
+
+        <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+        />
       </div>
     );
     // return React.createElement('div', {className: App}, React.createElement('h1', null, 'From react.create element!!'))
