a66f29da3f130f9cb423b47ea9df81b7931dd026
shivam0anand <shivam_anand@outlook.com>
2020-03-12 18:05:00 +0530

    modified

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,7 +2,26 @@ import React, { Component } from "react";
 import Person from "./Person/Person";
 
 class Persons extends Component {
+  static getDerivedStateFromProps(props, state) {
+    console.log("[Persons.js] getDerivedStateFromProps");
+    return state;
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+  }
+
+  componentDidUpdate() {
+    console.log("[Persons.js] componentDidUpdate");
+  }
+
   render() {
+    console.log("[Persons.js] rendring...");
     return this.props.persons.map((person, index) => {
       return (
         <Person
